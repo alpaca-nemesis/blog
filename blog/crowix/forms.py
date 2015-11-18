4cc4d7c3bc064dd82a334b6472245d40affd1669
crowix <mdzhangkailai@gmail.com>
2015-11-18 18:15:29 +0800

    the registration system has been completed

--- blog/crowix/forms.py
+++ blog/crowix/forms.py
@@ -5,3 +5,15 @@ class messageForm(forms.Form):
 	mail = forms.CharField()
 	phone = forms.CharField()
 	content = forms.CharField()
+
+class loginForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+
+class userForm(forms.Form):
+	username = forms.CharField()
+	email = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+	password2= forms.CharField(label='Confirm',widget=forms.PasswordInput)
+	def pwd_validate(self,p1,p2):
+		return p1==p2
--- blog/crowix/views.py
+++ blog/crowix/views.py
@@ -1,19 +1,27 @@
 #coding:utf-8
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import User
 from .models import artical
 from .models import message
-from .forms import messageForm
+from .forms import messageForm,loginForm,userForm
+
+
 
 # Create your views here.
 def index(request):
 	a1 = artical.objects.filter(slug="1")
 	a2 = artical.objects.filter(slug="2")
 	a3 = artical.objects.filter(slug="3")
+	f1 = loginForm()
+	f2 = userForm()
 	return render(request, 'index.html',{
 		'artical1': a1,
 		'artical2': a2,
 		'artical3': a3,
+		'form1': f1,
+		'form2': f2,
 		})
 
 def contact(request):
@@ -34,7 +42,47 @@ def contact(request):
 			return HttpResponse('hehe')
 	else:
 		f = messageForm()
-		return render(request, 'contact.html', {'form': f})
+		f1 = loginForm()
+		f2 = userForm()
+		return render(request, 'contact.html', {
+			'form': f,
+			'form1': f1,
+			'form2': f2,
+			})
 
 def term(request):
-	return render(request, 'terms.html')
+	f1 = loginForm()
+	f2 = userForm()
+	return render(request, 'terms.html',{
+		'form1': f1,
+		'form2': f2,
+		})
+
+def login(request):
+	if request.method == 'POST':
+		usernm = request.POST.get('username')
+		passwd = request.POST.get('password')
+		match = auth.authenticate(username=usernm, password=passwd)
+		if match:
+			auth.login(request, match)
+			return HttpResponse('You have logged in!')
+		else:
+			return HttpResponse('Something wrong!')
+
+def logout(request):
+	auth.logout(request)
+	return HttpResponseRedirect('/index')
+
+def regist(request):
+	if request.method == 'POST':
+		usernm = request.POST.get('username')
+		mail = request.POST.get('email')
+		passwd = request.POST.get('password')
+		passwd2 = request.POST.get('password2')
+		if passwd == passwd2:
+			newuser = User.objects.create_user(usernm,mail,passwd)
+			newuser.is_active = True  
+			newuser.save()
+			return HttpResponse('You have signed in!')
+		else:
+			return HttpResponse('you failed')
